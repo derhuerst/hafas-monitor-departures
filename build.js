7bca2a4c8c32814bc39d6e729723f3095616c36a
Jannis R <mail@jannisr.de>
2016-06-16 22:42:05 +0200

    rewrite build script

--- build.js
+++ build.js
@@ -1,40 +1,40 @@
 'use strict'
 
 const ndjson = require('ndjson')
-const rfs = require('rotating-file-stream')
+const fs = require('fs')
 const hafas = require('vbb-hafas')
 const stations = require('vbb-stations')
+const cfg = require('./config')
 
 
 
-const interval = 10
-const filename = (t, i) => {
-	if (!t) return 'db.ndjson'
-	return `db-${i}.ndjson`
+const fetch = (id, db) => {
+	let i = 0
+	const self = () => {
+		if (++i < cfg.iterations) setTimeout(self, cfg.interval * 60 * 1000)
+
+		const when = new Date(Date.now() + 60 * 1000)
+		hafas.departures(id, {when, duration: cfg.interval}).then((deps) => {
+
+			console.info(i, id)
+			for (let dep of deps) db.write({
+				  w:  dep.when / 1000
+				, d:  'delay' in dep ? dep.delay / 1000 : null
+				, s:  dep.station.id
+				, l:  dep.product.line
+				, dr: dep.direction
+			})
+
+		}, console.error)
+	}
+	return self
 }
+
+
+
 const db = ndjson.stringify()
-db.pipe(rfs(filename, {size: '1G', compress: true}))
+db.pipe(fs.createWriteStream('data.ndjson'))
 
-const fetch = (id) => () => {
-	const when = new Date(Date.now() + 60 * 1000)
-	hafas.departures(id, {when, duration: interval})
-	.then((deps) => {
-		console.info(id)
-		for (let dep of deps) db.write({
-			  w:  dep.when / 1000
-			, d:  'delay' in dep ? dep.delay / 1000 : null
-			, s:  dep.station.id
-			, p:  dep.product.line
-			, dr: dep.direction
-		})
-	}, console.error)
-}
-
-
-
+let x = 0
 stations('all').on('error', console.error)
-.on('data', (s) => {
-	const f = fetch(s.id)
-	setInterval(f, interval * 60 * 1000)
-	f()
-})
+.on('data', (s) => setTimeout(fetch(s.id, db), x++ * 100)) // spread load
---
+++ config.js
@@ -0,0 +1,6 @@
+'use strict'
+
+module.exports = {
+	interval:   1, // in minutes
+	iterations: 3
+}
