a66776501c42d8430a9b6fdf4e38dfe44e402e29
Jannis R <mail@jannisr.de>
2016-08-02 20:42:50 +0200

    tests :white_check_mark:

--- test.js
+++ test.js
@@ -27,4 +27,80 @@ const mockedHafas = () => ({departures: sinon.spy(mockedDepartures)})
 
 
 
-// todo
+test('returns a stream', (t) => {
+	t.plan(2)
+	const s1 = monitor(stations, interval)
+	const s2 = monitor(stations)
+
+	t.ok(isStream(s1))
+	t.ok(isStream(s2))
+
+	s1.stop()
+	s2.stop()
+})
+
+
+
+test('starts querying in steps', (t) => {
+	t.plan(3)
+	const clock = sinon.useFakeTimers()
+	const mock = mockedHafas()
+
+	const s = monitor(stations, interval, 50, mock)
+	t.equal(mock.departures.callCount, 0)
+	clock.tick(1)
+	t.equal(mock.departures.callCount, 1)
+	clock.tick(50)
+	t.equal(mock.departures.callCount, 2)
+
+	s.stop()
+	clock.restore()
+})
+
+
+
+test('checks for every `interval` milliseconds', (t) => {
+	t.plan(3)
+	const clock = sinon.useFakeTimers()
+	const mock = mockedHafas()
+
+	const s = monitor(stations, interval, 1, mock)
+	t.equal(mock.departures.callCount, 0)
+	clock.tick(1)
+	t.equal(mock.departures.callCount, stations.length)
+	clock.tick(interval)
+	t.equal(mock.departures.callCount, 2 * stations.length)
+
+	s.stop()
+	clock.restore()
+})
+
+
+
+test('clears all intervals on `stop()`', (t) => {
+	t.plan(1)
+	const clock = sinon.useFakeTimers()
+	const mock = mockedHafas()
+
+	const s = monitor(stations, interval, 1, mock)
+	clock.tick(1 + 5 * interval)
+	const count = mock.departures.callCount
+
+	s.stop()
+	clock.tick(5 * interval)
+	t.equal(mock.departures.callCount, count)
+
+	clock.restore()
+})
+
+
+
+test('emits `close` on `stop()`', (t) => {
+	t.plan(1)
+	const s = monitor(stations, interval)
+	const spy = sinon.spy()
+	s.on('close', spy)
+
+	s.stop()
+	t.equal(spy.callCount, 1)
+})
