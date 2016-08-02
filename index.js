d86a4c71f48d661a75149c3f8494131a3223d7c2
Jannis R <mail@jannisr.de>
2016-08-02 19:57:46 +0200

    bugfixes :bug:, allow mocking

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const hafas = require('vbb-hafas')
 
 
 
-const fetch = (id, duration, out) => () => {
+const fetch = (client, id, duration, out) => () => {
 	const when = new Date(Date.now() + 60 * 1000)
-	hafas.departures(id, {when, duration})
+	client.departures(id, {when, duration})
 	.then((deps) => {
 		for (let dep of deps) out.push({
 			  when: dep.when
@@ -22,9 +22,13 @@ const fetch = (id, duration, out) => () => {
 
 
 
-module.exports = (stations, interval = 60 * 1000) => {
+module.exports = (stations, interval, step, client) => {
 	if (!stations || stations.length === 0)
 		throw new Error('At least one station must be passed.')
+	interval = interval || 60 * 1000
+	step = step || Math.min(Math.floor(interval / stations.length), 100)
+	client = client || hafas // allow mocking
+	const duration = Math.ceil(interval / 60 / 1000)
 
 
 
@@ -38,22 +42,20 @@ module.exports = (stations, interval = 60 * 1000) => {
 
 
 	const intervals = {} // by station id
+	const timeouts = {} // by station id, used in the beginning
 
 	const start = () => {
-		const step = Math.min(Math.floor(interval / stations.length), 100)
-		let delay = 0
-		stations.forEach((id) => {
-			if (intervals[id]) clearInterval(intervals[id])
-			setTimeout(() => {
-				const cb = fetch(id, interval / 60 / 1000, out)
+		stations.forEach((id, i) => {
+			timeouts[id] = setTimeout(() => {
+				const cb = fetch(client, id, duration, out)
 				intervals[id] = setInterval(cb, interval)
 				cb()
-			}, delay)
-			delay += step
+			}, i * step)
 		})
 	}
 
 	const stop = () => {
+		for (let id in timeouts) clearTimeout(timeouts[id])
 		for (let id in intervals) clearInterval(intervals[id])
 	}
 
