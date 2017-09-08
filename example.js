ef08664d5e872e429af62172b6407a11263dff93
Jannis R <mail@jannisr.de>
2017-09-09 01:31:10 +0200

    expose average query duration, 1.2.0

--- example.js
+++ example.js
@@ -6,10 +6,11 @@ const alexanderplatz = '900000100003'
 const stations = [alexanderplatz] // array of station ids
 const interval = 2 * 60 * 1000 // every two minutes
 
-monitor(stations, interval)
+const departures = monitor(stations, interval)
 .on('error', console.error)
 .on('data', console.log)
+.on('avg-duration', console.error)
 
 setTimeout(() => {
 	departures.stop() // stop querying
-}, interval * 3)
+}, interval * 30)
--- index.js
+++ index.js
@@ -3,14 +3,6 @@
 const {Readable} = require('stream')
 const hafas = require('vbb-hafas')
 
-const fetch = (client, id, duration, out) => () => {
-	const when = new Date(Date.now() + 60 * 1000)
-	client.departures(id, {when, duration})
-	.then((deps) => {
-		for (let dep of deps) out.push(dep)
-	}, (err) => out.emit('error', err))
-}
-
 const monitor = (stations, interval, step, client) => {
 	if (!stations || stations.length === 0) {
 		throw new Error('At least one station must be passed.')
@@ -19,6 +11,27 @@ const monitor = (stations, interval, step, client) => {
 	step = step || Math.min(Math.floor(interval / stations.length), 100)
 	client = client || hafas // allow mocking
 	const duration = Math.ceil(interval / 60 / 1000)
+
+	let sumOfDurations = 0
+	let reqs = 0
+	const fetch = (client, id, duration, out) => () => {
+		const start = Date.now()
+		const when = new Date(start + 60 * 1000)
+
+		client.departures(id, {when, duration})
+		.then((deps) => {
+			sumOfDurations += Date.now() - start
+			reqs++
+
+			for (let dep of deps) out.push(dep)
+
+			if (reqs >= 10) {
+				const avg = Math.round(sumOfDurations / reqs)
+				sumOfDurations = reqs = 0
+				out.emit('avg-duration', avg)
+			}
+		}, err => out.emit('error', err))
+	}
 
 	const intervals = {} // by station id
 	const timeouts = {} // by station id, used in the beginning
